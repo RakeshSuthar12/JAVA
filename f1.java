71d5cc057cbb7143108da21b777136f584d76f70
RakeshSuthar12 <rakeshsutharise2025@gmail.com>
2025-11-02 12:58:47 +0530

    Update f1.java

--- f1.java
+++ f1.java
@@ -2,5 +2,6 @@ class f1{
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("Devops");
+        System.out.println("Jenkins")
     }
 }
